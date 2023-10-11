516d671b8602eea6d94e50edf193bd78cac2a68b
Imesh-Isuranga <imeshisuranga00@gmail.com>
2023-10-11 22:47:34 +0530

    Added secure communication with https for backend and frontend

--- Angular-FrontEnd/All4Store/src/app/config/my-app-config.ts
+++ Angular-FrontEnd/All4Store/src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
     oidc:{
         clientId:'0oaclwlucfBQk73RR5d7',
         issuer:'https://dev-12957119.okta.com/oauth2/default',
-        redirectUri:'http://localhost:4200/login/callback',
+        redirectUri:'https://localhost:4200/login/callback',
         scopes:['openid','profile','email']
     }
 }
--- Spring-Boot-BackEnd/spring-boot-ecommerce/src/main/java/com/store4all/ecommerce/dao/OrderRepository.java
+++ Spring-Boot-BackEnd/spring-boot-ecommerce/src/main/java/com/store4all/ecommerce/dao/OrderRepository.java
@@ -10,5 +10,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface OrderRepository extends JpaRepository<Order,Long> {
 
-    Page<Order> findByCustomerEmailOrdOrderByDateCreatedDesc(@Param("email") String email, Pageable pageable);
+    Page<Order> findByCustomerEmailOrderByDateCreatedDesc(@Param("email") String email, Pageable pageable);
 }
